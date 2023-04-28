dbb623dc6ed0689e787c1b7ae651f7be96f907b3
kendricknotken <70722711+kendricknotken@users.noreply.github.com>
2023-04-28 14:47:44 -0400

    Changed to php file

--- levelDesign.php
+++ levelDesign.php
@@ -1,3 +1,6 @@
+<?php 
+	$page="levelDesign"
+?>;
 <!DOCTYPE HTML>
 <!--
 	Telephasic by HTML5 UP
@@ -22,31 +25,7 @@
 							<h1 id="logo"><a href="index.html">Telephasic</a></h1>
 
 						<!-- Nav -->
-							<nav id="nav">
-								<ul>
-									<li>
-										<a href="#">Dropdown</a>
-										<ul>
-											<li><a href="#">Lorem ipsum dolor</a></li>
-											<li><a href="#">Magna phasellus</a></li>
-											<li><a href="#">Etiam dolore nisl</a></li>
-											<li>
-												<a href="#">Phasellus consequat</a>
-												<ul>
-													<li><a href="#">Lorem ipsum dolor</a></li>
-													<li><a href="#">Phasellus consequat</a></li>
-													<li><a href="#">Magna phasellus</a></li>
-													<li><a href="#">Etiam dolore nisl</a></li>
-												</ul>
-											</li>
-											<li><a href="#">Veroeros feugiat</a></li>
-										</ul>
-									</li>
-									<li><a href="left-sidebar.html">Left Sidebar</a></li>
-									<li class="break"><a href="right-sidebar.html">Right Sidebar</a></li>
-									<li><a href="no-sidebar.html">No Sidebar</a></li>
-								</ul>
-							</nav>
+							<?php include("navBar.php")?>;
 
 					</div>
 				</div>
@@ -70,6 +49,7 @@
 										<img src="images/levelDesign/Library 1.png" alt="Screenshot of Library" width="500"/>
 										<img src="images/levelDesign/Storage 1.png" alt="Screenshot of Storage" width="500"/>
 										<img src="images/levelDesign/Storage 2.png" alt="Screenshot of Storage image 2" width="500"/>
+								</center>
 								<p> As the team's only Level Designer I was tasked with creating every level and using
 								what gameplay mechanics we implemented to create space for the player to explore and play.
 								I created two levels comprised of multiple sections. Level 1 included the Square and Basement.
